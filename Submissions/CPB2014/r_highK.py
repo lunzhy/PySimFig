9bbf68da69d66958a8daf14a8e9b1275e0dd0e98
Workstation <lunzhy.work@gmail.com>
2014-05-21 19:56:06 +0800

    add new file and get ready for merging

---
+++ Submissions/CPB2014/r_highK.py
@@ -0,0 +1,28 @@
+__author__ = 'lunzhy'
+import os, sys
+path = os.path.abspath(os.path.join(os.path.abspath(os.path.dirname(__file__)), os.pardir, os.pardir))
+if not path in sys.path:
+    sys.path.append(path)
+from Submissions.CPB2014 import *
+import matplotlib.pyplot as plt
+import lib.common as comm
+
+
+def plot2DOcc():
+    return
+
+
+def plotVerticalCut():
+    return
+
+
+def plotLateralCut():
+    return
+
+
+def main():
+    return
+
+
+if __name__ == '__main__':
+    main()
